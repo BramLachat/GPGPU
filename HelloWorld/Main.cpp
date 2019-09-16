4c679d7f7781d57ee5aba3f4701139f336f2bc7e
BramLachat <bla.datacare@gmail.com>
2019-09-16 19:32:44 +0200

    Commit HelloWorld directory

---
+++ HelloWorld/Main.cpp
@@ -0,0 +1,25 @@
+#include <cassert>
+#include <iostream>
+
+#include "parse_stl.h"
+
+int main(int argc, char* argv[]) {
+	std::string stl_file_name = "./Box1x1x1.stl";
+
+	if (argc == 2) {
+		stl_file_name = argv[1];
+	}
+	else if (argc > 2) {
+		std::cout << "ERROR: Too many command line arguments" << std::endl;
+	}
+
+	auto info = stl::parse_stl(stl_file_name);
+
+	std::vector<stl::triangle> triangles = info.triangles;
+	std::cout << "STL HEADER = " << info.name << std::endl;
+	std::cout << "# triangles = " << triangles.size() << std::endl;
+	for (auto t : info.triangles) {
+		std::cout << t << std::endl;
+	}
+
+}
