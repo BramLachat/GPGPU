35220c4ca8ebb4107f68a7da159313c81490330f
Bram Lachat <bla@datacare.be>
2019-10-27 20:37:39 +0100

    _v6 = page-locked/pinned memory + gpu startup kernel added + Versiebeheer.txt

---
+++ RayTriangleGPU_v6/Main.cu
@@ -0,0 +1,260 @@
+#include <cassert>
+#include <iostream>
+#include <chrono>
+#include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+#include <thrust/device_vector.h>
+//#include <memory> //needed for smart pointers
+
+#include "Mesh.h"
+#include "parse_stl.h"
+#include "RayTriangleIntersect.cuh"
+
+void rayTriangleIntersect(float dir[3], std::unique_ptr<Mesh>& innerMesh, std::unique_ptr<Mesh>& outerMesh);
+void handleCudaError(cudaError_t cudaERR);
+__global__ void startGPU();
+
+int main(int argc, char* argv[]) {
+	std::string stl_file_inside;
+	std::string stl_file_outside;
+	int RayTriangle;
+	int CPU;
+	std::cout << "Enter filename of inside mesh:" << std::endl;
+	std::cin >> stl_file_inside;
+	std::cout << "Enter filename of outside mesh:" << std::endl;
+	std::cin >> stl_file_outside;
+	std::cout << "0 = RayTriangleIntersection, 1 = TriangleTriangleIntersection" << std::endl;
+	std::cin >> RayTriangle;
+	std::cout << "CPU? (yes = 1, no = 0)" << std::endl;
+	std::cin >> CPU;
+
+	if (argc == 2) {
+		stl_file_inside = argv[1];
+	}
+	else if (argc > 2) {
+		std::cout << "ERROR: Too many command line arguments" << std::endl;
+	}
+
+	auto t1 = std::chrono::high_resolution_clock::now(); //start time measurement
+
+	//Only reads STL-file in binary format!!!
+	std::cout << "lezen" << std::endl;
+	std::unique_ptr<Mesh> triangleMesh_Inside = stl::parse_stl(stl_file_inside);
+	std::unique_ptr<Mesh> triangleMesh_Outside = stl::parse_stl(stl_file_outside);
+
+	auto t2 = std::chrono::high_resolution_clock::now(); //stop time measurement
+	auto time = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+	std::cout << "Time = " << time << "ms" << std::endl;
+
+	std::cout << "STL HEADER = " << triangleMesh_Inside->getName() << std::endl;
+	std::cout << "# triangles = " << triangleMesh_Inside->getNumberOfTriangles() << std::endl;
+	std::cout << "# vertices = " << triangleMesh_Inside->getNumberOfVertices() << std::endl;
+
+	//triangleMesh_Inside.schrijf();
+
+	std::cout << "STL HEADER = " << triangleMesh_Outside->getName() << std::endl;
+	std::cout << "# triangles = " << triangleMesh_Outside->getNumberOfTriangles() << std::endl;
+	std::cout << "# vertices = " << triangleMesh_Outside->getNumberOfVertices() << std::endl;
+
+	//triangleMesh_Outside.schrijf();
+
+	Vertex* V1 = triangleMesh_Outside->getVertexAtIndex(0);
+	Vertex* V2 = triangleMesh_Outside->getVertexAtIndex(1);
+	Vertex* V3 = triangleMesh_Outside->getVertexAtIndex(2);
+
+	float xCenter = (V1->getCoordinates()[0] + V2->getCoordinates()[0] + V3->getCoordinates()[0])/3;
+	float yCenter = (V1->getCoordinates()[1] + V2->getCoordinates()[1] + V3->getCoordinates()[1])/3;
+	float zCenter = (V1->getCoordinates()[2] + V2->getCoordinates()[2] + V3->getCoordinates()[2])/3;
+
+	float direction[3] = { xCenter, yCenter, zCenter };
+	//float direction[3] = { 1.0, 1.0, 1.0 };
+
+	std::cout << "direction = " << direction[0] << ", " << direction[1] << ", " << direction[2] << std::endl;
+
+	//auto start = std::chrono::high_resolution_clock::now(); //start time measurement
+
+	if (RayTriangle == 0)
+	{
+		if (CPU == 1)
+		{
+			//2 opties om unique ptr mee te geven als argument aan een functie:
+			//https://stackoverflow.com/questions/30905487/how-can-i-pass-stdunique-ptr-into-a-function
+			triangleMesh_Outside->rayTriangleIntersect(direction, triangleMesh_Inside);
+		}
+		rayTriangleIntersect(direction, triangleMesh_Inside, triangleMesh_Outside);
+	}
+	else
+	{
+		triangleMesh_Outside->triangleTriangleIntersect(triangleMesh_Inside);
+	}	
+	
+	//auto end = std::chrono::high_resolution_clock::now(); //stop time measurement
+	//auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+	//std::cout << "Time = " << duration << "ms" << std::endl;
+
+	std::cout << "Press Enter to quit program!" << std::endl;
+	std::cin.get();
+	std::cin.get();
+	return 0;
+}
+
+void rayTriangleIntersect(float dir[3], std::unique_ptr<Mesh>& innerMesh, std::unique_ptr<Mesh>& outerMesh)
+{
+	auto start = std::chrono::high_resolution_clock::now(); //start time measurement
+	startGPU<<<1,1>>>();
+	cudaDeviceSynchronize();
+	auto end = std::chrono::high_resolution_clock::now(); //stop time measurement
+	auto transferDuration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+	std::cout << "\t\t\tStartup time GPU = " << transferDuration << "ms" << std::endl;
+
+	std::cout << "\t\t\tCalculating intersections! (GPU)" << std::endl;
+	std::cout << "--- Data Transfer ---" << std::endl;
+	start = std::chrono::high_resolution_clock::now(); //start time measurement
+
+	bool inside = true;
+	int numberOfOutsideTriangles = outerMesh->getNumberOfTriangles();
+	int numberOfInsideVertices = innerMesh->getNumberOfVertices();
+
+	//nodig om in kernel te controleren dat aantal keer dat test wordt uitgevoerd <= is dan het aantal driehoeken
+	//int numberOfCudaCalculations = numberOfInsideVertices * numberOfOutsideTriangles;
+
+	//bool* threadResult;
+	//handleCudaError(cudaMalloc((void**)& threadResult, numberOfCudaCalculations*sizeof(bool)));
+
+	/*thrust::host_vector<float3> insideOrigins = innerMesh->getVerticesVector();
+	thrust::device_vector<float3> cudaInsideOrigins(insideOrigins.size());
+	cudaInsideOrigins = insideOrigins;
+	thrust::device_ptr<float3> d_cudaInsideOrigins = cudaInsideOrigins.data();
+	float3* deviceInsideOrigins = thrust::raw_pointer_cast(d_cudaInsideOrigins);*/
+
+	float3* insideOrigins = innerMesh->getFloat3ArrayVertices();
+	float3* cudaInsideOrigins;
+	int sizeInsideVertices = numberOfInsideVertices * sizeof(float3);
+	//handleCudaError(cudaMallocManaged((void**)& cudaInsideOrigins, sizeInsideVertices));
+	//memcpy(cudaInsideOrigins, insideOrigins, sizeInsideVertices);
+	handleCudaError(cudaMalloc((void**)& cudaInsideOrigins, sizeInsideVertices));
+	handleCudaError(cudaMemcpy(cudaInsideOrigins, insideOrigins, sizeInsideVertices, cudaMemcpyHostToDevice));
+	
+	float* cudaDir;
+	//handleCudaError(cudaMallocManaged((void**)& cudaDir, 3*sizeof(float)));
+	//memcpy(cudaDir, dir, 3 * sizeof(float));
+	handleCudaError(cudaMalloc((void**)& cudaDir, 3 * sizeof(float)));
+	handleCudaError(cudaMemcpy(cudaDir, dir, 3*sizeof(float), cudaMemcpyHostToDevice));
+
+	/*thrust::host_vector<int3> outsideTriangles = outerMesh->getTrianglesVector();;
+	thrust::device_vector<int3> cudaOutsideTriangles(outsideTriangles.size());
+	cudaOutsideTriangles = outsideTriangles;
+	thrust::device_ptr<int3> d_cudaOutsideTriangles = cudaOutsideTriangles.data();
+	int3* deviceOutsideTriangles = thrust::raw_pointer_cast(d_cudaOutsideTriangles);*/
+
+	int3* outsideTriangles = outerMesh->getInt3ArrayTriangles();
+	int3* cudaOutsideTriangles;
+	int sizeOutsideTriangles = numberOfOutsideTriangles * sizeof(int3);
+	//handleCudaError(cudaMallocManaged((void**)& cudaOutsideTriangles, sizeOutsideTriangles));
+	//memcpy(cudaOutsideTriangles, outsideTriangles, sizeOutsideTriangles);
+	handleCudaError(cudaMalloc((void**)& cudaOutsideTriangles, sizeOutsideTriangles));
+	handleCudaError(cudaMemcpy(cudaOutsideTriangles, outsideTriangles, sizeOutsideTriangles, cudaMemcpyHostToDevice));
+
+	/*thrust::host_vector<float3> outsideVertices = outerMesh->getVerticesVector();
+	thrust::device_vector<float3> cudaOutsideVertices(outsideVertices.size());
+	cudaOutsideVertices = outsideVertices;
+	thrust::device_ptr<float3> d_cudaOutsideVertices = cudaOutsideVertices.data();
+	float3* deviceOutsideVertices = thrust::raw_pointer_cast(d_cudaOutsideVertices);*/
+
+	float3* outsideVertices = outerMesh->getFloat3ArrayVertices();
+	float3* cudaOutsideVertices;
+	int sizeOutsideVertices = outerMesh->getNumberOfVertices() * sizeof(float3);
+	//handleCudaError(cudaMallocManaged((void**)& cudaOutsideVertices, sizeOutsideVertices));
+	//memcpy(cudaOutsideVertices, outsideVertices, sizeOutsideVertices);
+	handleCudaError(cudaMalloc((void**)& cudaOutsideVertices, sizeOutsideVertices));
+	handleCudaError(cudaMemcpy(cudaOutsideVertices, outsideVertices, sizeOutsideVertices, cudaMemcpyHostToDevice));
+	
+	//thrust::device_vector<int> intersectionsPerOrigin(numberOfInsideVertices);
+	//int* d_intersectionsPerOrigin = thrust::raw_pointer_cast(&intersectionsPerOrigin[0]);
+	int* intersectionsPerOrigin = new int[numberOfInsideVertices];
+	int* cudaIntersectionsPerOrigin;
+	handleCudaError(cudaMalloc((void**)& cudaIntersectionsPerOrigin, numberOfInsideVertices*sizeof(int)));
+
+	//thrust::device_vector<float3> resultVertices(numberOfInsideVertices);
+	//float3* d_resultVertices = thrust::raw_pointer_cast(&resultVertices[0]);
+	float3* resultVertices = new float3[numberOfInsideVertices];
+	float3* cudaResultVertices;
+	handleCudaError(cudaMalloc((void**)& cudaResultVertices, numberOfInsideVertices * sizeof(float3)));
+
+	int totalIntersections = 0;
+
+	std::cout << "--- End Data Transfer ---" << std::endl;
+	end = std::chrono::high_resolution_clock::now(); //stop time measurement
+	transferDuration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+	std::cout << "\t\t\tTime Data Transfer = " << transferDuration << "ms" << std::endl;
+
+	std::cout << "--- Calculating ---" << std::endl;
+	start = std::chrono::high_resolution_clock::now(); //start time measurement
+
+	int numberOfBlocks = ((int)(numberOfInsideVertices + 511 / 512));
+	Intersection::intersect_triangleGPU<<<numberOfBlocks,512>>>(cudaInsideOrigins, cudaDir, cudaOutsideTriangles, cudaOutsideVertices, numberOfInsideVertices, numberOfOutsideTriangles, cudaIntersectionsPerOrigin, cudaResultVertices);
+	cudaError_t err = cudaGetLastError();
+	handleCudaError(err);
+
+	//std::vector<int> h_intersectionsPerOrigin(intersectionsPerOrigin.size());
+	//hrust::copy(intersectionsPerOrigin.begin(), intersectionsPerOrigin.end(), h_intersectionsPerOrigin.begin());
+	handleCudaError(cudaMemcpy(intersectionsPerOrigin, cudaIntersectionsPerOrigin, numberOfInsideVertices * sizeof(int), cudaMemcpyDeviceToHost));
+
+	//std::vector<float3> h_resultVertices(resultVertices.size());
+	//thrust::copy(resultVertices.begin(), resultVertices.end(), h_resultVertices.begin());
+	handleCudaError(cudaMemcpy(resultVertices, cudaResultVertices, numberOfInsideVertices * sizeof(float3), cudaMemcpyDeviceToHost));
+
+	std::unique_ptr<std::vector<Vertex>> verticesToWrite = std::make_unique<std::vector<Vertex>>();
+	verticesToWrite->reserve(numberOfInsideVertices);
+	float x, y, z;
+	for (int i = 0; i < numberOfInsideVertices; i++)
+	{
+		totalIntersections += intersectionsPerOrigin[i];
+		x = resultVertices[i].x;
+		y = resultVertices[i].y;
+		z = resultVertices[i].z;
+		if (x + y + z != 0) 
+		{
+			verticesToWrite->emplace_back(x, y, z); 
+			inside = false;
+		}
+	}
+
+	std::cout << "--- End Calculating ---" << std::endl;
+	end = std::chrono::high_resolution_clock::now(); //stop time measurement
+	auto calculatingDuration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+	std::cout << "\t\t\tTime Calculating = " << calculatingDuration << "ms" << std::endl;
+	std::cout << "\t\t\tTotal Time GPU = " << calculatingDuration+ transferDuration << "ms" << std::endl;
+
+	std::cout << "Writing to file!" << std::endl;
+	innerMesh->writeVerticesToFile(verticesToWrite, "OutsideVerticesCUDA.stl");
+
+	cudaFree(cudaInsideOrigins);
+	cudaFree(cudaDir);
+	cudaFree(cudaOutsideTriangles);
+	cudaFree(cudaOutsideVertices);
+	cudaFree(cudaIntersectionsPerOrigin);
+	cudaFree(cudaResultVertices);
+	cudaFreeHost(insideOrigins);
+	cudaFreeHost(outsideTriangles);
+	cudaFreeHost(outsideVertices);
+	//delete insideOrigins;
+	//delete outsideTriangles;
+	//delete outsideVertices;
+	delete intersectionsPerOrigin;
+	delete resultVertices;
+
+	std::cout << "totaal intersecties: " << totalIntersections << std::endl;
+	if (inside) { std::cout << "INSIDE" << std::endl; }
+	else { std::cout << "OUTSIDE" << std::endl; }
+}
+
+void handleCudaError(cudaError_t cudaERR) {
+	if (cudaERR != cudaSuccess) {
+		printf("CUDA ERROR : %s\n", cudaGetErrorString(cudaERR));
+	}
+}
+
+__global__ void startGPU() {
+	printf("GPU ready!\n");
+}
---
+++ RayTriangleGPU_v6/Mesh.cpp
@@ -0,0 +1,333 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <fstream>
+#include <chrono>
+
+#include "Mesh.h"
+#include "RayTriangleIntersect.cuh"
+#include "TriangleTriangleIntersect.h"
+
+
+Mesh::Mesh(std::string n, unsigned int size)
+{
+	name = n;
+	triangles.reserve(size);
+	vertices.reserve(size);
+}
+std::string Mesh::getName()
+{
+	return name;
+}
+int Mesh::getNumberOfTriangles()
+{
+	return triangles.size();
+}
+int Mesh::getNumberOfVertices()
+{
+	return vertices.size();
+}
+void Mesh::addTriangle(const Triangle& t)
+{
+	triangles.push_back(t);
+}
+void Mesh::addVertex(const Vertex& v)
+{
+	vertices.push_back(v);
+}
+int Mesh::findDuplicate(const Vertex& v)
+{
+	std::map<std::string, int>::iterator itr = VertexIndices.find(v.toString());
+	if (itr != VertexIndices.end())
+	{
+		return itr->second;
+	}
+	else
+	{
+		return -1;
+	}
+
+	/*std::vector<Vertex>::iterator it = std::find(vertices.begin(), vertices.end(), v);
+	if (it != vertices.end())//returned index of last value if nothing found!
+	{
+		return std::distance(vertices.begin(), it);
+	}
+	else
+	{
+		return -1;
+	}*/
+}
+void Mesh::rayTriangleIntersect(float dir[3], std::unique_ptr<Mesh>& innerMesh)
+{
+	auto start = std::chrono::high_resolution_clock::now(); //start time measurement
+
+	//std::unique_ptr<float[]> orig = std::make_unique<float[]>(3); //smart pointer
+	//std::vector<float> orig;
+	float* t = new float;
+	float* u = new float;
+	float* v = new float;
+	Vertex* V1;
+	Vertex* V2;
+	Vertex* V3;
+	float* vert1;
+	float* vert2;
+	float* vert3;
+	float* orig;
+	//float dirPerPoint[3];
+
+	Vertex* innerVertex;
+
+	std::unique_ptr<std::vector<Vertex>> outsideVertices = std::make_unique<std::vector<Vertex>>();
+
+	bool inside = true;
+	int totalIntersections = 0;
+
+	std::cout << "\t\t\tCalculating intersections! (CPU)" << std::endl;
+
+	for(int j = 0 ; j < innerMesh->getNumberOfVertices() ; j++)
+	{
+		innerVertex = &(innerMesh->vertices.at(j));
+		orig = innerVertex->getCoordinates();
+		//dirPerPoint[0] = dir[0] - orig[0];
+		//dirPerPoint[1] = dir[1] - orig[1];
+		//dirPerPoint[2] = dir[2] - orig[2];
+		//std::cout << "orig = " << orig[0] << ", " << orig[1] << ", " << orig[2] << std::endl;
+
+		int numberOfIntersections = 0;
+
+		for (int i = 0; i < triangles.size(); i++)
+		{
+			V1 = &(vertices.at(triangles.at(i).getIndexOfVertexInMesh(0)));
+			V2 = &(vertices.at(triangles.at(i).getIndexOfVertexInMesh(1)));
+			V3 = &(vertices.at(triangles.at(i).getIndexOfVertexInMesh(2)));
+			vert1 = V1->getCoordinates();
+			vert2 = V2->getCoordinates();
+			vert3 = V3->getCoordinates();
+			//if (Intersection::intersect_triangle3(orig, dirPerPoint, vert1, vert2, vert3, t, u, v) == 1)
+			if (Intersection::intersect_triangleCPU(orig, dir, vert1, vert2, vert3, t, u, v) == 1)
+			{
+				numberOfIntersections++;
+				//std::cout << "1, ";
+			}
+			else {
+				//std::cout << "0, ";
+			}
+		}
+		totalIntersections += numberOfIntersections;
+		//std::cout << "aantal intersecties = " << numberOfIntersections << std::endl;
+		if (numberOfIntersections % 2 == 0)
+		{
+			inside = false;
+			outsideVertices->push_back(*innerVertex);
+		}
+	}
+
+	auto end = std::chrono::high_resolution_clock::now(); //stop time measurement
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+	std::cout << "\t\t\tTime CPU = " << duration << "ms" << std::endl;
+
+	std::cout << "totaal intersecties = " << totalIntersections << std::endl;
+	delete t; delete u; delete v;
+	if (inside) { std::cout << "INSIDE" << std::endl; }
+	else { std::cout << "OUTSIDE" << std::endl; }
+
+	std::cout << "Writing to file!" << std::endl;
+	writeVerticesToFile(outsideVertices, "OutsideVertices.stl");
+}
+void Mesh::triangleTriangleIntersect(std::unique_ptr<Mesh>& innerMesh)
+{
+	auto start = std::chrono::high_resolution_clock::now(); //start time measurement
+
+	float* vert1_1;
+	float* vert1_2;
+	float* vert1_3;
+	float* vert2_1;
+	float* vert2_2;
+	float* vert2_3;
+	Triangle* t1;
+	Triangle* t2;
+	std::vector<Vertex>* innerVertices = &(innerMesh->vertices);
+
+	std::unique_ptr<std::vector<Triangle>> intersectingTriangles1 = std::make_unique<std::vector<Triangle>>();
+	std::unique_ptr<std::vector<Triangle>> intersectingTriangles2 = std::make_unique<std::vector<Triangle>>();
+
+	bool inside = true;
+
+	std::cout << "\t\t\tCalculating intersecting triangles! (CPU)" << std::endl;
+
+	for (int j = 0; j < innerMesh->getNumberOfTriangles(); j++)
+	{
+		t1 = &(innerMesh->triangles.at(j));
+		vert1_1 = innerVertices->at(t1->getIndexOfVertexInMesh(0)).getCoordinates();
+		vert1_2 = innerVertices->at(t1->getIndexOfVertexInMesh(1)).getCoordinates();
+		vert1_3 = innerVertices->at(t1->getIndexOfVertexInMesh(2)).getCoordinates();
+
+		int numberOfIntersections = 0;
+
+		for (int i = 0; i < triangles.size(); i++)
+		{
+			t2 = &(triangles.at(i));
+			vert2_1 = vertices.at(t2->getIndexOfVertexInMesh(0)).getCoordinates();
+			vert2_2 = vertices.at(t2->getIndexOfVertexInMesh(1)).getCoordinates();
+			vert2_3 = vertices.at(t2->getIndexOfVertexInMesh(2)).getCoordinates();
+			if (Intersection::NoDivTriTriIsect(vert1_1, vert1_2, vert1_3, vert2_1, vert2_2, vert2_3) == 1)
+			{
+				//list printed with intersecting triangles
+				intersectingTriangles1->push_back(*t1);
+				intersectingTriangles2->push_back(*t2);
+
+				numberOfIntersections++;
+			}
+		}
+		//std::cout << "aantal intersecties = " << numberOfIntersections << std::endl;
+		if (numberOfIntersections != 0)
+		{
+			inside = false;
+		}
+	}
+	if (inside) { std::cout << "SNIJDEN NIET" << std::endl; }
+	else { std::cout << "SNIJDEN WEL" << std::endl; }
+
+	auto end = std::chrono::high_resolution_clock::now(); //stop time measurement
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+	std::cout << "\t\t\tTime CPU = " << duration << "ms" << std::endl;
+
+	std::cout << "Writing to file!" << std::endl;
+	writeTrianglesToFile(intersectingTriangles1, innerVertices, "IntersectingTriangles1.stl");
+	writeTrianglesToFile(intersectingTriangles2, &vertices, "IntersectingTriangles2.stl");
+}
+int Mesh::getLastVertex()
+{
+	return (vertices.size() - 1);
+}
+void Mesh::schrijf()
+{
+	for (int i = 0; i < triangles.size(); i++)
+	{
+		std::cout << "driehoek " << i << std::endl;
+		for (int j = 0; j < 3; j++)
+		{
+			std::cout << "vertex " << j << ": index = " << triangles.at(i).getIndexOfVertexInMesh(j) << std::endl;
+			Vertex v = vertices.at(triangles.at(i).getIndexOfVertexInMesh(j));
+			v.schrijf();
+		}
+		std::cout << std::endl;
+	}
+	std::map<std::string, int>::iterator itr;
+	for (itr = VertexIndices.begin(); itr != VertexIndices.end(); itr++)
+	{
+		std::cout << "key: " << itr->first  // string (key)
+			<< ':'
+			<< "value: " << itr->second   // string's value 
+			<< std::endl;
+	}
+}
+Vertex* Mesh::getVertexAtIndex(int index)
+{
+	return &(vertices.at(index));
+}
+void Mesh::resize()
+{
+	vertices.shrink_to_fit();
+}
+void Mesh::addVertexIndex(const std::string& s, int index)
+{
+	VertexIndices.insert(std::pair<std::string, int>(s, index));
+}
+void Mesh::writeTrianglesToFile(std::unique_ptr<std::vector<Triangle>>& triangles, std::vector<Vertex>* vertices, std::string fileName)
+{
+	std::vector<Triangle>::iterator itr;
+	//std::string path = "C:\\Users\\hla\\Documents\\Masterproef\\GPGPU\\Output\\" + fileName;
+	std::string path = "D:\\Masterproef\\GPGPU\\Output\\" + fileName;
+	std::ofstream ofs(path);
+	ofs << "solid IntersectingTriangles" << std::endl;
+	for (itr = triangles->begin(); itr != triangles->end(); ++itr)
+	{
+		ofs << "  facet normal  0.0  0.0  0.0" << std::endl;
+		ofs << "    outer loop" << std::endl;
+		float* vert;
+		for (int j = 0; j < 3; j++)
+		{
+			vert = vertices->at(itr->getIndexOfVertexInMesh(j)).getCoordinates();
+			ofs << "      vertex  " << vert[0] << "  "
+				<< vert[1] << "  "
+				<< vert[2] << std::endl;
+		}
+		ofs << "    endloop" << std::endl;
+		ofs << "  endfacet" << std::endl;
+	}
+	ofs << "endsolid vcg" << std::endl;
+}
+int3* Mesh::getInt3ArrayTriangles()
+{
+	int3* triangleArray;
+	cudaError_t status = cudaHostAlloc((void**)& triangleArray, triangles.size() * sizeof(int3), cudaHostAllocDefault);
+	if (status != cudaSuccess) printf("Error allocating pinned host memory\n");
+	//int3* triangleArray = new int3[triangles.size()];
+	for (int i = 0 ; i < triangles.size() ; i++)
+	{
+		triangleArray[i] = triangles[i].getIndexOfVerticesInMesh();
+	}
+	return triangleArray;
+}
+thrust::host_vector<int3> Mesh::getTrianglesVector()
+{
+	thrust::host_vector<int3> result(triangles.size());
+	for (int i = 0; i < triangles.size(); i++) 
+	{
+
+		result[i] = triangles[i].getIndexOfVerticesInMesh();
+	}
+	return result;
+}
+float3* Mesh::getFloat3ArrayVertices()
+{
+	float3* vertexArray;
+	cudaError_t status = cudaHostAlloc((void**)& vertexArray, vertices.size() * sizeof(float3), cudaHostAllocDefault);
+	if (status != cudaSuccess) printf("Error allocating pinned host memory\n");
+	//float3* vertexArray = new float3[vertices.size()];
+	for (int i = 0; i < vertices.size(); i++)
+	{
+		vertexArray[i] = vertices[i].getCoordinatesFloat3();
+	}
+	return vertexArray;
+}
+thrust::host_vector<float3> Mesh::getVerticesVector()
+{
+	thrust::host_vector<float3> result(vertices.size());
+	for (int i = 0; i < vertices.size(); i++)
+	{
+
+		result[i] = vertices[i].getCoordinatesFloat3();
+	}
+	return result;
+}
+void Mesh::writeVerticesToFile(std::unique_ptr<std::vector<Vertex>>& vertices, std::string fileName)
+{
+	std::vector<Vertex>::iterator itr;
+	//std::string path = "C:\\Users\\hla\\Documents\\Masterproef\\GPGPU\\Output\\" + fileName;
+	std::string path = "D:\\Masterproef\\GPGPU\\Output\\" + fileName;
+	std::ofstream ofs(path);
+	ofs << "solid IntersectingTriangles" << std::endl;
+	float* vert;
+	for (itr = vertices->begin(); itr != vertices->end(); ++itr)
+	{
+		ofs << "  facet normal  0.0  0.0  0.0" << std::endl;
+		ofs << "    outer loop" << std::endl;
+		vert = itr->getCoordinates();
+		ofs << "      vertex  " << vert[0] << "  "
+			<< vert[1] << "  "
+			<< vert[2] << std::endl;
+		ofs << "      vertex  " << (vert[0]-0.1) << "  "
+			<< (vert[1]-0.1) << "  "
+			<< (vert[2]-0.1) << std::endl;
+		ofs << "      vertex  " << (vert[0] + 0.1) << "  "
+			<< (vert[1] + 0.1) << "  "
+			<< (vert[2] - 0.1) << std::endl;
+		ofs << "    endloop" << std::endl;
+		ofs << "  endfacet" << std::endl;
+	}
+	ofs << "endsolid vcg" << std::endl;
+}
