5296e618e7fdfd1e652ae4519c5bf14bcd45b669
Bram Lachat <bla@datacare.be>
2019-11-26 15:31:05 +0100

    Triangle-Triangle algorithm implementation GPU

---
+++ TriangleTriangle_v1/Triangle.h
@@ -0,0 +1,20 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+#include <vector>
+
+#include "Vertex.h"
+
+class Triangle
+{
+private:
+	int vertexIndexes[3];
+public:
+	Triangle();
+	Triangle(const Triangle& t);
+	void addVertexIndex(int index, int vertexNumber);
+	int getIndexOfVertexInMesh(int index);
+	int3 getIndexOfVerticesInMesh();
+	void clear();
+};
+#endif
---
+++ TriangleTriangle_v1/Vertex.cpp
@@ -0,0 +1,87 @@
+#include <memory>
+#include <iostream>
+
+#include "Vertex.h"
+
+int Vertex::copies = 0;
+Vertex::Vertex()
+{
+	point[0] = 0; point[1] = 0; point[2] = 0;
+}
+Vertex::Vertex(float x, float y, float z)
+{
+	//point = new float[3];
+	point[0] = x;
+	point[1] = y;
+	point[2] = z;
+}
+Vertex::Vertex(const Vertex& v) //copyconstructor
+{
+	//point = new float[3];
+//	memcpy(point, v.point, 3);
+	point[0] = v.point[0];
+	point[1] = v.point[1];
+	point[2] = v.point[2];
+	//std::cout << "Copied" << std::endl;
+	Vertex::copies++;
+}
+Vertex& Vertex::operator=(const Vertex& v) //copy-assignment
+{
+	if (this != &v) {
+		//if (point != nullptr) delete[] point;
+		//point = new float[3];
+		point[0] = v.point[0];
+		point[1] = v.point[1];
+		point[2] = v.point[2];
+		return *this;
+	}
+	else {
+		return *this;
+	}
+}
+bool Vertex::operator==(const Vertex& v)
+{
+	if (v.point[0] == point[0] && v.point[1] == point[1] && v.point[2] == point[2])
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+bool Vertex::isDuplicate(const Vertex& v) const
+{
+	if (v.point[0] == point[0] && v.point[1] == point[1] && v.point[2] == point[2])
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+float* Vertex::getCoordinates()
+{
+	return point;
+}
+float Vertex::getCoordinate(int i)
+{
+	return point[i];
+}
+float3 Vertex::getCoordinatesFloat3()
+{
+	return make_float3(point[0], point[1], point[2]);
+}
+/*Vertex::~Vertex()
+{
+	delete[] point;
+}*/
+void Vertex::schrijf()
+{
+	std::cout << point[0] << ", " << point[1] << ", " << point[2] << std::endl;
+}
+std::string Vertex::toString() const
+{
+	return std::to_string(point[0]) + std::to_string(point[1]) + std::to_string(point[2]);
+}
