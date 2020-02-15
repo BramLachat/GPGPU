5a3c314cd2dca47b8c08864e3aeaf099351ae830
Bram Lachat <bla@datacare.be>
2020-02-16 00:38:08 +0100

    New implementation of the TriangleTriangle algorithm (thread per (outer) triangle)

---
+++ TriangleTriangle_v3/TriangleTriangleIntersect.cuh
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cuda_runtime.h>
+
+__host__ __device__ int NoDivTriTriIsect(float V0[3], float V1[3], float V2[3],
+	float U0[3], float U1[3], float U2[3]);
+
+__host__ __device__ int coplanar_tri_tri(float N[3], float V0[3], float V1[3], float V2[3],
+	float U0[3], float U1[3], float U2[3]);
+
+__global__ void triangle_triangle_GPU_BPCD(int3* cudaInsideTriangles, float3* cudaInsideVertices, int3* cudaOutsideTriangles, float3* cudaOutsideVertices, bool* inside, int numberOfInsideTriangles, int numberOfOutsideTriangles, float2* cudaInsideTriangleIntervals);
+
+__global__ void triangle_triangle_GPU(int3* cudaInsideTriangles, float3* cudaInsideVertices, int3* cudaOutsideTriangles, float3* cudaOutsideVertices, bool* inside, int numberOfInsideTriangles, int numberOfOutsideTriangles);
